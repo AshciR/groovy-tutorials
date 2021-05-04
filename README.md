f3c66b7270815a1e437b9eed215e21ddd2dd2f09
Richard Walker <richard.walker.90@gmail.com>
2021-05-04 16:32:59 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Groovy Tutorial
+Welcome! This repository was created to help you
+get used to the Groovy language. 
+
+## Topics
---
+++ src/test/groovy/io/richie/HelloWorldSpec.groovy
@@ -0,0 +1,12 @@
+package io.richie
+
+import spock.lang.Specification
+
+class HelloWorldSpec extends Specification {
+
+    def 'Hello Spock World'() {
+        expect:
+        true
+    }
+
+}
