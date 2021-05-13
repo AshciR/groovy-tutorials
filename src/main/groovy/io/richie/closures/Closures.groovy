ffbc8e884e17a08f927d5c92fa2f63da08c8e939
Richard Walker <richard.walker.90@gmail.com>
2021-05-12 23:15:39 -0400

    Finished Closures

---
+++ src/main/groovy/io/richie/closures/Closures.groovy
@@ -0,0 +1,10 @@
+package io.richie.closures
+
+class Closures {
+
+    // This is a regular method
+    static Integer addTwoToValue(int value) {
+        value + 2
+    }
+
+}
---
+++ src/main/groovy/io/richie/closures/JavaOven.java
@@ -0,0 +1,17 @@
+package io.richie.closures;
+
+public class JavaOven {
+
+    public static String bakeFruitPie(String fruit) {
+        return "Baked into a delicious " + fruit + " pie!";
+    }
+
+    public static String bakeFruitTurnover(String fruit) {
+        return "Baked into a delicious " + fruit + " turnover!";
+    }
+
+    public static String bakeFruitMuffin(String fruit) {
+        return "Baked into a delicious " + fruit + " muffin!";
+    }
+
+}
